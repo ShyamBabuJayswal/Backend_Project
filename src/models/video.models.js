42f48476b19e823a694935c8d20ae9bb4fd4fe40
NIhal <jaisnj541@gmail.com>
2024-07-01 20:33:45 +0530

    add user and video models

---
+++ src/models/video.models.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type:String,//cloudnary url
+            required:true
+
+        },
+        thumbnail:{
+            type:String,
+            required:true
+
+        },
+        title:{
+            type:String,
+            required:true,
+        },
+        discription:{
+            type:String,
+             required:true,
+        },
+        duration:{
+            type:Number,
+            required:true,
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type:Boolean,
+            default:true
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+        }
+
+
+    },
+    {
+     timestamp:true,
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const  Video=mongoose.model("Video",videoSchema);
