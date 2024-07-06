ed848c58c3b44ab9962a60b6653f2900db68b1a2
NIhal <jaisnj541@gmail.com>
2024-07-06 11:49:16 +0530

    aggregation pipeline done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -325,6 +325,87 @@ throw new ApiError(401,error?.message|| "Invalid refresh token" )
          )
            
         })
+
+
+  const getUserChannelProfile  = asyncHandler(async(req,res) => {
+     const {username}=req.params  
+
+     if(!username?.trim()){
+        throw new ApiError(400,"username is missing")
+     }
+
+     const channel =  await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+            {
+                $lookup:{
+                    from:"subscriptions",
+                    localField:"_id",
+                    foreignField:"channel",
+                    as:"subscribers"
+                }
+            },
+            {
+                $lookup:{
+                    from:"subscriptions",
+                    localField:"_id",
+                    foreignField:"subscribers",
+                    as:"subscriberdTo"
+                }
+            },
+            {
+                $addFields:{
+                    subscribersCount:{
+                        $size:"$subscribers"
+                    },
+                    channelsSubscribedToCount:{
+                        $size:"$subscriberdTo"
+                    },
+                    isSubscribed:{
+                        $cond:{
+                           if:{
+                            $in:[req.user?._id,"$subscribers.subscriber"]
+                           },
+                           then:true,
+                           else:false
+                        
+                }
+                    }
+                }
+            },
+            {
+                $project:{
+                    fullName:1,
+                    username:1,
+                    subscribersCount:1,
+                    channelsSubscribedToCount:1,
+                    isSubscribed:1,
+                    avatar:1,
+                    coverImage:1,
+                    email:1
+                }
+            }
+        
+     ])
+
+    
+      if(!channel?.length){
+        throw new ApiError(404,"channel does not exists");
+      }
+
+      return res
+      .status(200)
+      .json(
+        new ApiResponse(200,channel[0],"User channel fetched succesfully")
+      )
+
+
+    }) 
+        
+
 export { registerUser,
      loginUser,
      logoutUser,
@@ -333,5 +414,6 @@ export { registerUser,
       getCurrentUser,
       updateAccountDetails,
       updateUserAvatar,
-      updateUserCoverImage
+      updateUserCoverImage,
+      getUserChannelProfile
     };
