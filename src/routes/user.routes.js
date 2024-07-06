88ca310c31b0bcaaedeb88bb7af903ed0141b2f9
NIhal <jaisnj541@gmail.com>
2024-07-06 13:37:43 +0530

    Add new Routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser,
+    logoutUser,
+     registerUser,
+      refreshAccessToken,
+       changeCurrentPassword,
+        getCurrentUser,
+         updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
+
+
 const router = Router();
 
-router.route("/register").post(
+router
+.route("/register")
+.post(
     upload.fields([
        {
         name:"avatar",
@@ -18,11 +28,41 @@ router.route("/register").post(
     ]),
     registerUser);
 
-     router.route("/login").post(loginUser)
+     router
+     .route("/login")
+     .post(loginUser)
 
   //secured route
 
-  router.route("/logout").post(verifyJWT,logoutUser)
-  router.route("/refresh-roken").post(refreshAccessToken)
+  router
+  .route("/logout")
+  .post(verifyJWT,logoutUser)
+
+  router
+  .route("/refresh-roken")
+  .post(refreshAccessToken)
+
+  router
+  .route("/change-password")
+  .post(verifyJWT,changeCurrentPassword)
+
+  router
+  .route("/current-user")
+  .get(verifyJWT,getCurrentUser)
+
+  router
+  .route("/update--account")
+  .patch(verifyJWT,updateAccountDetails)
+
+  router
+  .route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+  router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+
+  router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+  router.route("/history").get(verifyJWT,getWatchHistory)
+
+
 
 export default router;
