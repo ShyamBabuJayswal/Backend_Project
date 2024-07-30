a79021d6632a9a9dd63807e54833bdea02818008
NIhal <jaisnj541@gmail.com>
2024-07-30 08:11:16 +0530

    dashboard

---
+++ src/routes/dashbaord.routes.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import {like} from "../models/like.models.js"
+import {Subscription} from "../models/subscription.model.js"
+import {Like} from "../models/like.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getChannelStats = asyncHandler(async(req,res)  => {
+
+    
+})
